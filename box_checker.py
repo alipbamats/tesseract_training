b01a02c9f7dbe7455ffa3094ba7ab73a3fa0453e
alipbamats <alipbamats@gmail.com>
2026-07-18 07:10:22 -0700

    Update box_checker.py

--- box_checker.py
+++ box_checker.py
@@ -25,10 +25,10 @@ for root, dirs, files in os.walk(args.dir):
             for box_line in box_text.split(b'\n'):
                 match=re.search(rb"(.*)\s(.*)\s(.*)\s(.*)\s(.*)\s(.*)",box_line)
                 if match:
-                    print(match.group(1).hex(),match.group(1).decode("utf-8"))
+                    print("-->","\"{}\"-\"{}\"".format(match.group(1).hex(),match.group(1).decode("utf-8")))
                     box_items = [match.group(i) for i in range(1,7)]
                     if box_items[0].hex() == "d380":
                         box_items[0]=b"\x49"
-                        print("-->EDIT")
+                        print("-->#EDIT")
                     new_box_line=b" ".join(box_items)+b"\n"
                     out_file.write(new_box_line)
