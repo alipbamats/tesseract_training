128f25684727fb7b0a238c682708d132b8331a0a
alipbamats <alipbamats@gmail.com>
2026-08-16 05:03:55 -0700

    Update box_checker.py

--- box_checker.py
+++ box_checker.py
@@ -27,19 +27,19 @@ for root, dirs, files in os.walk(args.dir):
                 if match:
                     box_items = [match.group(i) for i in range(1,7)]
                     if box_items[0].hex() =="49":
-                        box_items[0]=b"\xd3\x80"
+                        box_items[0]=b"\xd3\x80" #декодируется в кириллическую букву Ӏ (палочка, U+04C0 CYRILLIC LETTER PALOCHKA).
                         if args.verbose:
                             print("-->", "sym: \"{}\", code: \"{}\", ##EDIT".format(match.group(1).decode("utf-8"), match.group(1).hex()))
                         else:
                             print("-->#EDIT")
                     elif box_items[0].hex() =="27":
-                        box_items[0]=b"\xcc\x81"
+                        box_items[0]=b"\xcc\x81"  #комбинируемый знак ударения (Combining Acute Accent), который в стандарте Unicode имеет код U+0301 (́).
                         if args.verbose:
                             print("-->", "sym: \"{}\", code: \"{}\", ##EDIT".format(match.group(1).decode("utf-8"), match.group(1).hex()))
                         else:
                             print("-->#EDIT")
                     elif box_items[0].hex() =="3d":
-                        box_items[0]=b"\xcc\x84"
+                        box_items[0]=b"\xcc\x84" #U+0305 — комбинируемую надчеркивающую черту
                         if args.verbose:
                             print("-->", "sym: \"{}\", code: \"{}\", ##EDIT".format(match.group(1).decode("utf-8"), match.group(1).hex()))
                         else:
